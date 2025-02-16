4c0434dd399911fe1570d8aa330fde0683559ad0
robinmuff <robin.muff@bluewin.ch>
2025-02-16 23:09:06 +0100

    Structure setup file

--- docker-ollama-setup.sh
+++ docker-ollama-setup.sh
@@ -1,3 +1,6 @@
+# Start container
 docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 
-docker exec -it ollama ollama run llama3
+# Models installation
+docker exec -it ollama ollama run llama3 # 4 GB
+docker exec -it ollama ollama run llama3.3 # 42 GB
