8a99db70205ddb7c61ca8b97cea0501ad699ed05
robinmuff <robin.muff@bluewin.ch>
2025-02-16 20:35:06 +0100

    Initial commit

---
+++ docker-ollama-setup.sh
@@ -0,0 +1,3 @@
+docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+docker exec -it ollama ollama run llama3
---
+++ model-related-infos.md
@@ -0,0 +1,5 @@
+# Ollama docker infos
+https://hub.docker.com/r/ollama/ollama
+
+# Ollama library for models
+https://ollama.com/library
